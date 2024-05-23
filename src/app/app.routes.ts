c42573000cff01c6609ad1f0ad90c402c50747b7
shashikanth <shashikanthreddy270@gmail.com>
2024-05-23 14:05:58 +0530

    add products module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//   {
+//     path: 'opportunities',
+//     loadChildren: () =>
+//       import('./opportunities').then((m) => m.OpportunitiesModule),
+//   },
+  {
+    path: 'products',
+    loadChildren: () => import('./products/products.module').then((m) => m.ProductsModule),
+  },
+//   {
+//     path: 'maintenance',
+//     component: MaintenanceComponent,
+//   },
+];
